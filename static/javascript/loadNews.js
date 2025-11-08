f6bee2e5de257edc9d434c98d9cbd94804c3cb28
Basster284 <meclersasa370@gmail.com>
2025-11-08 14:27:03 +0500

    Removed console.log from loadNews.js

--- static/javascript/loadNews.js
+++ static/javascript/loadNews.js
@@ -2,7 +2,6 @@ async function loadnews(){
     const resp = await fetch("/getrianews").then(response => response.json()).then(data => JSON.stringify(data));
     const data = JSON.parse(resp).data;
     Object.entries(data).forEach(i => {
-        console.log(i)
         const ndiv = document.createElement("div");
         ndiv.style = `background-image: linear-gradient(to top, rgba(0, 0, 0, 1) 0%, rgba(0, 0, 0, 0) 100%), url(${i[1].image});`
         ndiv.style.cursor = "pointer";
